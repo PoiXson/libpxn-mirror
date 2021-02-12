6483abd8b0e2eb323a491b5f62a80eca11478df1
lorenzop <lorenzo@poixson.com>
2021-02-11 22:53:24 -0500

    rewrote tpl-engine.c with many improvements

--- src/tpl-engine.c
+++ src/tpl-engine.c
@@ -1,16 +1,11 @@
 #include "tpl-engine.h"
 
+#include <stdlib.h>
 #include <string.h>
 
-#include "../utils/files.h"
-#include "../utils/strings.h"
-
-#if __has_include("../utils/logs.h")
-#include "../utils/logs.h"
-#endif
-#if __has_include("../api/web11api.h")
-#include "../api/web11api.h"
-#endif
+#include "StringUtils.h"
+#include "FileUtils.h"
+#include "Logs.h"
 
 
 
@@ -18,80 +13,51 @@
 
 
 
-TPL_Doc* tpl_load_doc(char *file) {
+TPL_Doc* tpl_doc_load(const char *file) {
 	char *data;
 	int len = load_text_file(file, &data);
 	if (len < 0)
 		return NULL;
+	return tpl_doc_parse(data);
+}
+
+TPL_Doc* tpl_doc_parse(char *data) {
 	TPL_Doc *doc = calloc(1, sizeof(TPL_Doc));
-	doc->nodes_size = tpl_parse(&(doc->nodes), data);
+	tpl_doc_init(doc);
+	doc->nodes_size = tpl_parse(doc, &(doc->nodes), data);
 	return doc;
 }
 
 
 
-size_t tpl_new_node(TPL_Node **nodes, size_t *nodes_size) {
-	// find space in existing array
-	if ((*nodes)[(*nodes_size)-1].type == 0) {
-		size_t index = (*nodes_size) - 1;
-		while (true) {
-			index--;
-			if ((*nodes)[index].type != 0)
-				return index + 1;
-			if (index == 0)
-				break;
-		}
-		return 0;
-	}
-	// expand array
-	(*nodes_size) *= 2;
-	*nodes = realloc(*nodes, sizeof(TPL_Node) * (*nodes_size));
-	if (*nodes == NULL) {
-		log_fatal("Failed to reallocate TPL Nodes array");
-		exit(1);
-	}
-	for (size_t index=(*nodes_size)/2; index<(*nodes_size); index++) {
-		tpl_init_node(nodes, index);
-	}
-	return tpl_new_node(nodes, nodes_size);
-}
-
-void tpl_init_node(TPL_Node **nodes, size_t index) {
-	(*nodes)[index].type = 0;
-	(*nodes)[index].data_size  = 0;
-	(*nodes)[index].nodes_size = 0;
-}
-
-
-
-size_t tpl_parse(TPL_Node **nodes, char *data) {
+size_t tpl_parse(TPL_Doc *doc, TPL_Node **nodes, char *data) {
 	if (*nodes != NULL) {
 		log_fatal("nodes array already allocated");
 		exit(1);
 	}
-	// allocate nodes array
-	size_t nodes_size = 2;
-	*nodes = calloc(nodes_size, sizeof(TPL_Node));
-	if (*nodes == NULL) {
-		log_fatal("Failed to allocate TPL Nodes array");
-		exit(1);
-	}
-	for (size_t index=0; index<nodes_size; index++) {
-		tpl_init_node(nodes, index);
-	}
-	// parse data
 	size_t data_len = strlen(data);
+	#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+	size_t line_count = 0;
+	for (size_t index=0; index<data_len; index++) {
+		if (data[index] == '\n')
+			line_count++;
+	}
+	if (data[data_len-1] != '\n')
+		line_count++;
+	log_line("");
+	log_line("Parsing [%lu] bytes, [%lu] lines..", data_len, line_count);
+	#endif
+	// parse data
 	size_t pos  = 0;
 	size_t last = 0;
+	size_t nodes_size = 0;
+	size_t nodes_count = 0;
 	//PARSE_LOOP:
-#ifdef TPL_DETAIL_LOG
-	log_detail("Parsing %lu bytes..", data_len);
-#endif
 	while (true) {
 		if (pos >= data_len-1) break;
 		if (data[pos] == '\0') break;
 
-		// parse tag
+		// parse tag {...}
 		if (data[pos] == '{') {
 
 			// {* comment tag *}
@@ -101,20 +67,24 @@ size_t tpl_parse(TPL_Node **nodes, char *data) {
 				while (true) {
 					if (pos >= data_len-1) break;
 					if (data[pos] == '\0') break;
+					// *}
 					if (data[pos] == '*' && data[pos+1] == '}') {
 						// store comment
-						// note: include empty comments
-						//if (pos > last) {
-							size_t index = tpl_new_node(nodes, &nodes_size);
-							size_t len = (pos - last) + 1;
-							(*nodes)[index].type = type_comment;
-							(*nodes)[index].data_size = len;
-							(*nodes)[index].text = calloc(len, sizeof(char));
-							strlcpy( (*nodes)[index].text, &(data[last]), len );
-#ifdef TPL_DETAIL_LOG
-							log_detail("  <Comment: %lu bytes>", len);
-#endif
-						//}
+						size_t index = tpl_node_allocate(nodes, &nodes_size);
+						nodes_count++;
+						size_t len = (pos - last) + 1;
+						(*nodes)[index].type = NODE_TYPE_COMMENT;
+						char *str_o = calloc(len, sizeof(char));
+						strlcpy(str_o, &(data[last]), len);
+						char *str_t = str_trim(str_o);
+						len = strlen(str_t) + 1;
+						(*nodes)[index].var.value_size = len;
+						(*nodes)[index].var.value = calloc(len, sizeof(char));
+						strlcpy((*nodes)[index].var.value, str_t, len);
+						free(str_o);
+						#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+						log_line("  <Comment: %lu bytes> %.20s", len, (*nodes)[index].var.value);
+						#endif
 						pos += 2;
 						break;
 					}
@@ -130,18 +100,25 @@ size_t tpl_parse(TPL_Node **nodes, char *data) {
 				while (true) {
 					if (pos >= data_len-1) break;
 					if (data[pos] == '\0') break;
+					// }}
 					if (data[pos] == '}' && data[pos+1] == '}') {
 						// store variable name
 						if (pos > last) {
-							size_t index = tpl_new_node(nodes, &nodes_size);
+							size_t index = tpl_node_allocate(nodes, &nodes_size);
+							nodes_count++;
 							size_t len = (pos - last) + 1;
-							(*nodes)[index].type = type_var;
-							(*nodes)[index].data_size = len;
-							(*nodes)[index].var_name = calloc(len, sizeof(char));
-							strlcpy( (*nodes)[index].var_name, &(data[last]), len );
-#ifdef TPL_DETAIL_LOG
-							log_detail("  <Var: %s>", (*nodes)[index].var_name);
-#endif
+							(*nodes)[index].type = NODE_TYPE_VAR;
+							char *str_o = calloc(len, sizeof(char));
+							strlcpy(str_o, &(data[last]), len);
+							char *str_t = str_trim(str_o);
+							len = strlen(str_t) + 1;
+							(*nodes)[index].var.name_size = len;
+							(*nodes)[index].var.name = calloc(len, sizeof(char));
+							strlcpy( (*nodes)[index].var.name, str_t, len );
+							free(str_o);
+							#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+							log_line("  <Var: %s>", (*nodes)[index].var.name);
+							#endif
 						}
 						pos += 2;
 						break;
@@ -152,27 +129,69 @@ size_t tpl_parse(TPL_Node **nodes, char *data) {
 			}
 
 			// {% statement tag %}
-			// include / if / elseif / endif / loop / endloop / break / continue
+			// include / set / if / loop ...
 			if (data[pos+1] == '%') {
 				pos += 2;
 				last = pos;
 				while (true) {
 					if (pos >= data_len-1) break;
 					if (data[pos] == '\0') break;
+					// %}
 					if (data[pos] == '%' && data[pos+1] == '}') {
-						// store the statement
-						if (pos > last) {
-							size_t index = tpl_new_node(nodes, &nodes_size);
-							size_t len = (pos - last) + 1;
-//TODO: find statement type
-(*nodes)[index].type = type_loop;
-							(*nodes)[index].data_size = len;
-(*nodes)[index].expr = calloc(len, sizeof(char));
-							strlcpy( (*nodes)[index].expr, &(data[last]), len );
-#ifdef TPL_DETAIL_LOG
-							log_detail("  <Statement: %s bytes>", (*nodes)[index].expr);
-#endif
+
+						// {% set variable = .. %}
+						size_t content_size = (pos - last) + 1;
+						char content[content_size];
+						strlcpy(content, &(data[last]), content_size);
+						char *content_t = str_trim(content);
+						if (str_starts_with(content_t, "set ")) {
+							content_t += 4;
+							size_t pos = chrpos(content_t, '=');
+							// var = ""
+							if (pos == -1) {
+								log_warning("Unknown variable value: %s", content);
+//TODO: is this right?
+								pos += 2;
+								break;
+							}
+							content_t[pos] = '\0';
+							char *var_name  = content_t;
+							char *var_value = content_t + pos + 1;
+							var_name  = str_trim(var_name);
+							var_value = str_trim(var_value);
+							size_t val_len = strlen(var_value);
+							if ( (var_value[0] == '"' && var_value[val_len-1] == '"') ||
+							(var_value[0] == '\'' && var_value[val_len-1] == '\'') ) {
+								var_value[val_len-1] = '\0';
+								var_value++;
+							}
+							// store set variable
+							size_t index = tpl_node_allocate(nodes, &nodes_size);
+							nodes_count++;
+							(*nodes)[index].type = NODE_TYPE_SET_VAR;
+							size_t len_name  = strlen(var_name)+1;
+							size_t len_value = strlen(var_value)+1;
+							(*nodes)[index].var.name_size  = len_name;
+							(*nodes)[index].var.value_size = len_value;
+							(*nodes)[index].var.name  = calloc(len_name,  sizeof(char));
+							(*nodes)[index].var.value = calloc(len_value, sizeof(char));
+							strlcpy( (*nodes)[index].var.name,  var_name,  len_name);
+							strlcpy( (*nodes)[index].var.value, var_value, len_value);
+							#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+							log_line("  <Set: %s = %s>", (*nodes)[index].var.name, (*nodes)[index].var.value);
+							#endif
 						}
+
+//						// store the statement
+//						if (pos > last) {
+//							size_t index = tpl_node_allocate(nodes, &nodes_size);
+//							size_t len = (pos - last) + 1;
+//TODO: find statement type
+//(*nodes)[index].type = NODE_TYPE_LOOP;
+//							(*nodes)[index].data_size = len;
+//(*nodes)[index].expr = calloc(len, sizeof(char));
+//							strlcpy( (*nodes)[index].expr, &(data[last]), len );
+//						}
 						pos += 2;
 						break;
 					}
@@ -181,95 +200,266 @@ size_t tpl_parse(TPL_Node **nodes, char *data) {
 				continue;
 			}
 
-		}
+		} // end parse tag {...}
 
 		// text until next tag
 		{
 			last = pos;
+			// TEXT_LOOP:
 			while (true) {
 				if (pos >= data_len-1) break;
 				if (data[pos] == '\0') break;
 				// found a tag
 				if (data[pos] == '{') {
-					if (data[pos+1] == '*' || data[pos+1] == '{' || data[pos+1] == '%') {
+					// {{ {* {%
+					if (data[pos+1] == '{' || data[pos+1] == '*' || data[pos+1] == '%') {
 						// store text
 						if (pos > last) {
-							size_t index = tpl_new_node(nodes, &nodes_size);
+							size_t index = tpl_node_allocate(nodes, &nodes_size);
+							nodes_count++;
 							size_t len = (pos - last) + 1;
-							(*nodes)[index].type = type_text;
-							(*nodes)[index].data_size = len;
-							(*nodes)[index].text = calloc(len, sizeof(char));
-							strlcpy( (*nodes)[index].text, &(data[last]), len );
-#ifdef TPL_DETAIL_LOG
-							log_detail("  <Text: %lu bytes>", len);
-#endif
+							(*nodes)[index].type = NODE_TYPE_TEXT;
+							(*nodes)[index].var.value_size = len;
+							(*nodes)[index].var.value = calloc(len, sizeof(char));
+							strlcpy( (*nodes)[index].var.value, &(data[last]), len );
+							#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+							log_line("  <Text: %lu bytes>", len);
+							#endif
 						}
 						break;
 					}
-				}
+				} // end found '{'
 				pos++;
-			}
-		}
+			} // end TEXT_LOOP
+		} // end text
 
 	} // end PARSE_LOOP
 	// remaining text
 	if (pos > last) {
 		if (pos < data_len)
 			pos++;
-		size_t index = tpl_new_node(nodes, &nodes_size);
+		size_t index = tpl_node_allocate(nodes, &nodes_size);
+		nodes_count++;
 		size_t len = (pos - last) + 1;
-		(*nodes)[index].type = type_text;
-		(*nodes)[index].data_size = len;
-		(*nodes)[index].text = calloc(len, sizeof(char));
-		strlcpy( (*nodes)[index].text, &(data[last]), len );
-#ifdef TPL_DETAIL_LOG
-		log_detail("  <Text: %lu bytes>", len);
-#endif
+		(*nodes)[index].type = NODE_TYPE_TEXT;
+		(*nodes)[index].var.value_size = len;
+		(*nodes)[index].var.value = calloc(len, sizeof(char));
+		strlcpy( (*nodes)[index].var.value, &(data[last]), len );
+		#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+		log_line("  <Text: %lu bytes>", len);
+		#endif
 	}
-	return nodes_size;
+	#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+	log_line("Parsed [%lu] nodes", nodes_count);
+	log_line("");
+	#endif
+	return nodes_count;
 }
 
-char* tpl_render(const TPL_Doc *doc) {
-	size_t result_size = 200;
-	char *result = calloc(result_size, sizeof(char));
-	if (result == NULL) {
+
+
+size_t tpl_node_allocate(TPL_Node **nodes, size_t *nodes_size) {
+	// allocate first block
+	if (*nodes_size == 0) {
+		*nodes_size = 2;
+		*nodes = malloc((*nodes_size) * sizeof(TPL_Node));
+		if (*nodes == NULL) {
+			log_fatal("Failed to allocate nodes array");
+			exit(1);
+		}
+		for (size_t index=0; index<*nodes_size; index++) {
+			tpl_node_init( &((*nodes)[index]) );
+		}
+		return 0;
+	}
+	// find space in existing array
+	for (size_t index=0; index<*nodes_size; index++) {
+		if ( ((*nodes)[index]).type == NODE_TYPE_UNUSED ) {
+			tpl_node_init( &((*nodes)[index]) );
+			return index;
+		}
+	}
+	// expand array
+	*nodes = realloc(*nodes, sizeof(TPL_Node) * (*nodes_size) * 2);
+	if (*nodes == NULL) {
+		log_fatal("Failed to reallocate nodes array");
+		exit(1);
+	}
+	for (size_t index=*nodes_size; index<(*nodes_size)*2; index++) {
+		tpl_node_init( &((*nodes)[index]) );
+	}
+	(*nodes_size) *= 2;
+	return tpl_node_allocate(nodes, nodes_size);
+}
+
+size_t tpl_var_allocate(TPL_Doc *doc) {
+	// first var
+	if (doc->vars_size == 0) {
+		doc->vars_size = 1;
+		doc->vars = malloc(doc->vars_size * sizeof(TPL_Var));
+		if (doc->vars == NULL) {
+			log_fatal("Failed to allocate tpl document vars array");
+			exit(1);
+		}
+		for (size_t index=0; index<doc->vars_size; index++) {
+			tpl_var_init( &(doc->vars[index]) );
+		}
+		return 0;
+	}
+	// find space in existing array
+	for (size_t index=0; index<doc->vars_size; index++) {
+		if (doc->vars[index].name_size == 0) {
+			tpl_var_init( &(doc->vars[index]) );
+			return index;
+		}
+	}
+	// expand array
+	doc->vars = realloc(doc->vars, sizeof(TPL_Var) * doc->vars_size);
+	if (doc->vars == NULL) {
+		log_fatal("Failed to reallocate tpl document vars array");
+		exit(1);
+	}
+	for (size_t index=doc->vars_size; index<doc->vars_size*2; index++) {
+		tpl_var_init( &(doc->vars[index]) );
+	}
+	doc->vars_size *= 2;
+	return tpl_var_allocate(doc);
+}
+
+
+
+void tpl_doc_init(TPL_Doc *doc) {
+	doc->render_comments = false;
+	doc->nodes_size = 0;
+	doc->vars_size  = 0;
+}
+
+void tpl_node_init(TPL_Node *node) {
+	node->type = NODE_TYPE_UNUSED;
+	node->nodes_size = 0;
+	node->var.name_size  = 0;
+	node->var.value_size = 0;
+}
+
+void tpl_var_init(TPL_Var *var) {
+	var->name_size  = 0;
+	var->value_size = 0;
+}
+
+
+
+char* tpl_get_var(TPL_Doc *doc, char *name) {
+	for (size_t index=0; index<doc->vars_size; index++) {
+		if (doc->vars[index].name_size == 0)
+			continue;
+		if (strlcmp(doc->vars[index].name, name, doc->vars[index].name_size) == 0)
+			return doc->vars[index].value;
+	}
+	return NULL;
+}
+
+void tpl_set_var(TPL_Doc *doc, char *name, char* value) {
+	// find existing variable
+	for (size_t index=0; index<doc->vars_size; index++) {
+		if (doc->vars[index].name_size == 0)
+			continue;
+		if (strlcmp(doc->vars[index].name, name, doc->vars[index].name_size) == 0) {
+			strlrcpy( &(doc->vars[index].value), value, &(doc->vars[index].value_size) );
+			#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+			log_line("  Changed variable: [%lu] %s = `%s`", index, name, value);
+			#endif
+			return;
+		}
+	}
+	// add new variable
+	{
+		size_t index = tpl_var_allocate(doc);
+		doc->vars[index].name_size  = strlen(name)  + 1;
+		doc->vars[index].value_size = strlen(value) + 1;
+		doc->vars[index].name  = calloc(doc->vars[index].name_size,  sizeof(char));
+		doc->vars[index].value = calloc(doc->vars[index].value_size, sizeof(char));
+		strlcpy(doc->vars[index].name,  name,  doc->vars[index].name_size);
+		strlcpy(doc->vars[index].value, value, doc->vars[index].value_size);
+		#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+		log_line("  Set variable: [%lu] %s = `%s`", index, name, value);
+		#endif
+	}
+}
+
+void tpl_dump_vars(TPL_Doc *doc) {
+	size_t count = 0;
+	for (size_t index=0; index<doc->vars_size; index++) {
+		count++;
+	}
+	log_line("Dumping [%lu] Var%s:", count, (count == 1 ? "" : "s"));
+	for (size_t index=0; index<doc->vars_size; index++) {
+		log_line("  %s = `%s`", doc->vars[index].name, doc->vars[index].value);
+	}
+}
+
+
+
+size_t tpl_render(char **result, TPL_Doc *doc) {
+	// allocate first block
+	size_t result_size = 100;
+	*result = calloc(result_size, sizeof(char));
+	if (*result == NULL) {
 		log_fatal("Failed to allocate for rendered output");
 		exit(1);
 	}
 	// render nodes
-	tpl_render_nodes(&result, &result_size, doc->nodes, doc->nodes_size);
-	return result;
+	#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+	log_line("Rendering Doc..");
+	#endif
+	tpl_render_nodes(doc, result, &result_size, doc->nodes, doc->nodes_size);
+	return result_size;
 }
 
-void tpl_render_nodes(char **result, size_t *result_size, TPL_Node *nodes, size_t nodes_size) {
+void tpl_render_nodes(TPL_Doc *doc, char **result, size_t *result_size, TPL_Node *nodes, size_t nodes_size) {
+	#if defined(DEBUG) && defined(TPL_DETAIL_LOG)
+	log_line("Rendering Nodes..");
+	#endif
 	// NODES_LOOP:
 	for (size_t index=0; index<nodes_size; index++) {
-		if (nodes[index].type == 0)
+		if (nodes[index].type == NODE_TYPE_UNUSED)
 			continue;
 		switch (nodes[index].type) {
-		case type_comment:
-			strlcat(*result, "/*", *result_size);
-			strlcat(*result, nodes[index].text, *result_size);
-			strlcat(*result, "*/", *result_size);
+		// {* comment *}
+		case NODE_TYPE_COMMENT:
+			if (doc->render_comments) {
+				strlrcat(result, "/* ", result_size);
+				strlrcat(result, nodes[index].var.value, result_size);
+				strlrcat(result, " */", result_size);
+			}
 			break;
-		case type_text:
-			strlcat(*result, nodes[index].text, *result_size);
+		// text
+		case NODE_TYPE_TEXT:
+			strlrcat(result, nodes[index].var.value, result_size);
 			break;
-		case type_var:
-			strlcat(*result, "<VAR:", *result_size);
-			strlcat(*result, nodes[index].var_name, *result_size);
-			strlcat(*result, ">", *result_size);
+		// {{ variable }}
+		case NODE_TYPE_VAR: {
+			char *value = tpl_get_var(doc, nodes[index].var.name);
+			if (value == NULL) {
+				log_warning("Rendering Node [%lu] Unknown variable: %s", index, nodes[index].var.name);
+				break;
+			}
+			strlrcat(result, value, result_size);
 			break;
-//TODO
-		case type_include:
-		case type_if:
-		case type_elseif:
-		case type_loop:
-		case type_endloop:
-		case type_break:
-		case type_continue:
-log_warning("Unfinished template node type! %i", nodes[index].type);
+		}
+		// {% set var=value %}
+		case NODE_TYPE_SET_VAR: {
+			if (nodes[index].var.name_size == 0) {
+				log_warning("Rendering Node [%lu] Unknown variable name", index);
+				break;
+			}
+			if (nodes[index].var.value_size == 0) {
+				log_warning("Rendering Node [%lu] Unknown variable value for: %s", index, nodes[index].var.name);
+				break;
+			}
+			tpl_set_var(doc, nodes[index].var.name, nodes[index].var.value);
 			break;
+		}
+		// unknown
 		default:
 			log_severe("Unknown template node type: %i", nodes[index].type);
 			break;
--- src/tpl-engine.h
+++ src/tpl-engine.h
@@ -1,55 +1,74 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 
 typedef enum TPL_Node_Type {
-	type_comment  = 0x1,
-	type_text     = 0x2,
-	type_var      = 0x3,
-	type_include  = 0x4,
-	type_if       = 0x5,
-	type_elseif   = 0x6,
-	type_endif    = 0x7,
-	type_loop     = 0x8,
-	type_endloop  = 0x9,
-	type_break    = 0xa,
-	type_continue = 0xb
+	NODE_TYPE_UNUSED   = 0x0,
+	NODE_TYPE_COMMENT  = 0x1,
+	NODE_TYPE_TEXT     = 0x2,
+	NODE_TYPE_VAR      = 0x3,
+	NODE_TYPE_SET_VAR  = 0x4
+//TODO
+//	NODE_TYPE_INCLUDE  = 0x,
+//	NODE_TYPE_IF       = 0x,
+//	NODE_TYPE_ELSE     = 0x,
+//	NODE_TYPE_ELSEIF   = 0x,
+//	NODE_TYPE_ENDIF    = 0x,
+//	NODE_TYPE_LOOP     = 0x,
+//	NODE_TYPE_ENDLOOP  = 0x,
+//	NODE_TYPE_CONTINUE = 0x,
+//	NODE_TYPE_BREAK    = 0x
 } TPL_Node_Type;
 
 
 
+typedef struct TPL_Var {
+	size_t name_size;
+	size_t value_size;
+	char *name;
+	char *value;
+} TPL_Var;
+
 typedef struct TPL_Node {
 	// node type
 	TPL_Node_Type type;
-	// node data
-	size_t data_size;
-	union {
-		char *text;     // type_text
-		char *var_name; // type_var
-		char *expr;     // type_if/elseif/loop
-	};
-	// child nodes
+
 	size_t nodes_size;
 	struct TPL_Node *nodes;
+
+	TPL_Var var;
 } TPL_Node;
 
 
 
 typedef struct TPL_Doc {
-	// doc nodes
-	TPL_Node *nodes;
+	bool render_comments;
 	size_t nodes_size;
+	TPL_Node *nodes;
+	size_t vars_size;
+	TPL_Var *vars;
 } TPL_Doc;
 
 
 
-TPL_Doc* tpl_load_doc(char *file);
+TPL_Doc* tpl_doc_load(const char *file);
+TPL_Doc* tpl_doc_parse(char *data);
 
-size_t tpl_new_node(TPL_Node **nodes, size_t *nodes_size);
-void tpl_init_node(TPL_Node **nodes, size_t index);
+size_t tpl_parse(TPL_Doc *doc, TPL_Node **nodes, char *data);
 
-size_t tpl_parse(TPL_Node **nodes, char *data);
-char* tpl_render(const TPL_Doc *doc);
-void tpl_render_nodes(char **result, size_t *result_size, TPL_Node *nodes, size_t nodes_size);
+size_t tpl_node_allocate(TPL_Node **nodes, size_t *nodes_size);
+size_t tpl_var_allocate(TPL_Doc *doc);
+
+void tpl_doc_init(TPL_Doc *doc);
+void tpl_node_init(TPL_Node *node);
+void tpl_var_init(TPL_Var *var);
+
+char* tpl_get_var(TPL_Doc *doc, char *name);
+void  tpl_set_var(TPL_Doc *doc, char *name, char* value);
+void  tpl_dump_vars(TPL_Doc *doc);
+
+size_t tpl_render(char **result, TPL_Doc *doc);
+void   tpl_render_nodes(TPL_Doc *doc, char **result, size_t *result_size, TPL_Node *nodes, size_t nodes_size);
