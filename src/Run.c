0168b1a4b97897c70e316fd5dafbdb9bee61ce42
lorenzop <lorenzo@poixson.com>
2021-02-21 03:20:54 -0500

    added new run_cmd_live() and parse_cmd_args() functions to Run.c

--- src/Run.c
+++ src/Run.c
@@ -1,5 +1,10 @@
 #include "Run.h"
 
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -50,4 +55,104 @@ RunResult* run_script(char *script) {
 	char cmd[PATH_MAX];
 	snprintf(cmd, PATH_MAX, "sh %s", script);
 	return run_cmd(cmd);
+}
+
+
+
+void parse_cmd_args(char *cmd, char **argv) {
+	while (*cmd != '\0') {
+		while (*cmd == ' ' || *cmd == '\t' || *cmd == '\n')
+			*cmd++ = '\0';
+		*argv++ = cmd;
+		while (true) {
+			if (*cmd == '\0' || *cmd == ' '
+			||  *cmd == '\t' || *cmd == '\n')
+				break;
+			cmd++;
+		}
+	}
+	*argv = '\0';
+}
+
+
+
+RunResult* run_cmd_live(char *cmd) {
+	log_detail("Running cmd: %s", cmd);
+	char *argv[64];
+	parse_cmd_args(cmd, argv);
+	// capture stdout
+	int handle_out[2];
+	pipe(handle_out);
+	const pid_t pid = fork();
+	if (pid < 0) {
+		log_fatal("Failed to fork a new thread");
+		return NULL;
+	}
+	// child thread
+	if (pid == 0) {
+		dup2(handle_out[1], 1); // replace stdout
+		dup2(handle_out[1], 2); // replace stderr
+		int handle_in = open("/dev/null", O_WRONLY);
+		dup2(handle_in,     0); // replace stdin
+		close(handle_in);
+		close(handle_out[0]);
+		close(handle_out[1]);
+		// execute
+		if (execvp(*argv, argv) < 0) {
+			printf("ERROR: exec failed: %i\n", errno);
+		}
+		exit(1);
+	}
+	// parent thread
+	close(handle_out[1]);
+	RunResult *result = calloc(1, sizeof(RunResult));
+	bool log_detail = is_level_loggable(LVL_DETAIL);
+	size_t buf_size = 64;
+	char buffer[buf_size];
+	size_t last_pos = 0;
+	while (true) {
+		ssize_t len = read(handle_out[0], buffer, buf_size);
+		if (len <= 0) {
+			if (errno == EAGAIN) continue;
+			if (errno == EINTR)  continue;
+			break;
+		}
+		strlrcat(&(result->out), &(result->out_size), buffer, len);
+		// live log
+		if (log_detail) {
+			while (true) {
+				size_t pos = chrposs(result->out, '\n', last_pos);
+				if (pos == -1)
+					break;
+				size_t line_len = (pos - last_pos) + 1;
+				char line[line_len];
+				strlcpy(line, result->out + last_pos, line_len);
+				log_line(" >%s", line);
+				last_pos = pos + 1;
+			}
+		} // end live log
+	}
+	close(handle_out[0]);
+	// finish live log
+	if (log_detail) {
+		if (result->out_size - 1 > last_pos) {
+			size_t line_len = result->out_size - last_pos;
+			char line[line_len];
+			strlcpy(line, result->out + last_pos, line_len);
+			log_line(" >%s", line);
+		}
+	} // end live log
+	// wait for death of child
+	int status;
+	while (true) {
+		int r = waitpid(pid, &status, 0);
+		if (r != -1)        break;
+		if (errno != EINTR) break;
+	}
+	status /= 256;
+	if (log_detail) {
+		log_line("cmd exit: %i", status);
+	}
+	result->status = status;
+	return result;
 }
--- src/Run.h
+++ src/Run.h
@@ -4,6 +4,7 @@
 
 
 
+#define CMD_ARGS_MAX 64
 // 10kb
 #define RUN_CMD_RESULT_MAX 1024 * 10
 
@@ -12,10 +13,13 @@
 typedef struct RunResult {
 	size_t out_size;
 	char *out;
-	int exit_code;
+	int status;
 } RunResult;
 
 
 
 RunResult* run_cmd(char *cmd);
 RunResult* run_script(char *script);
+
+void parse_cmd_args(char *cmd, char **argv);
+RunResult* run_cmd_live(char *cmd);
