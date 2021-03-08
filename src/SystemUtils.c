1b7783a2008b5a9f02f38f6b8d6f22f4d236d5b8
lorenzop <lorenzo@poixson.com>
2021-03-07 21:26:45 -0500

    added SystemUtils.c/h files

---
+++ src/SystemUtils.c
@@ -0,0 +1,23 @@
+#include "SystemUtils.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+
+void assert_user_root() {
+	uid_t user_id = getuid();
+	if (user_id != 0) {
+		printf("\nPermission denied; run as root\n\n");
+		exit(EACCES);
+	}
+}
+
+void assert_not_user_root() {
+	uid_t user_id = getuid();
+	if (user_id == 0) {
+		printf("\nPermission denied; run as a non-root user\n\n");
+		exit(EACCES);
+	}
+}
---
+++ src/SystemUtils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+
+void assert_user_root();
+void assert_not_user_root();
