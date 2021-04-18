a77a9a6d73d8244e2a7722f9ffe7dfdc36119ea2
lorenzop <lorenzo@poixson.com>
2021-04-18 19:56:50 -0400

    added dl-cjson.sh script and cJSON.c/h files

---
+++ dl-cjson.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+
+url="https://raw.githubusercontent.com/DaveGamble/cJSON"
+version="v1.7.14"
+
+
+[[ -d src/cjson/ ]] || \mkdir -pv "src/cjson/" || exit 1
+wget -v --unlink -O  "src/cjson/cJSON.c"    "$url/$version/cJSON.c"    || exit 1
+wget -v --unlink -O  "src/cjson/cJSON.h"    "$url/$version/cJSON.h"    || exit 1
+wget -v --unlink -O  "src/cjson/README.md"  "$url/$version/README.md"  || exit 1
+wget -v --unlink -O  "src/cjson/LICENSE"    "$url/$version/LICENSE"    || exit 1
