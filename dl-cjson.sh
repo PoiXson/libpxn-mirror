8b9dde7b0a33aea945e6ac2cdf2552293e795023
lorenzop <lorenzo@poixson.com>
2021-09-20 20:37:37 -0400

    minor fix to dl-cjson.sh script

--- dl-cjson.sh
+++ dl-cjson.sh
@@ -6,7 +6,7 @@ version="v1.7.15"
 
 
 [[ -d src/cjson/ ]] || \mkdir -pv "src/cjson/" || exit 1
-wget -v --unlink -O  "src/cjson/cJSON.c"    "$url/$version/cJSON.c"    || exit 1
-wget -v --unlink -O  "src/cjson/cJSON.h"    "$url/$version/cJSON.h"    || exit 1
-wget -v --unlink -O  "src/cjson/README.md"  "$url/$version/README.md"  || exit 1
-wget -v --unlink -O  "src/cjson/LICENSE"    "$url/$version/LICENSE"    || exit 1
+\wget -v --unlink -O  "src/cjson/cJSON.c"    "$url/$version/cJSON.c"    || exit 1
+\wget -v --unlink -O  "src/cjson/cJSON.h"    "$url/$version/cJSON.h"    || exit 1
+\wget -v --unlink -O  "src/cjson/README.md"  "$url/$version/README.md"  || exit 1
+\wget -v --unlink -O  "src/cjson/LICENSE"    "$url/$version/LICENSE"    || exit 1
