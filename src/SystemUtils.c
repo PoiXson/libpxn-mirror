8a57e94c61781c9beb3d8a3732213bcaf9098d48
lorenzop <lorenzo@poixson.com>
2021-11-06 22:09:03 -0400

    cleaned up and added is_root_user() to SystemUtils.c/h

--- src/SystemUtils.c
+++ src/SystemUtils.c
@@ -1,3 +1,21 @@
+//===============================================================================
+// Copyright (c) 2020-2021 PoiXson, Mattsoft
+// <https://poixson.com> <https://mattsoft.net>
+// Released under the GPL 3.0
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//===============================================================================
 #include "SystemUtils.h"
 
 #include <stdio.h>
@@ -7,17 +25,20 @@
 
 
 
-void assert_user_root() {
+bool is_root_user() {
 	uid_t user_id = getuid();
-	if (user_id != 0) {
+	return (user_id == 0);
+}
+
+void assert_user_root() {
+	if (is_root_user() != true) {
 		printf("\nPermission denied; run as root\n\n");
 		exit(EACCES);
 	}
 }
 
 void assert_not_user_root() {
-	uid_t user_id = getuid();
-	if (user_id == 0) {
+	if (is_root_user()) {
 		printf("\nPermission denied; run as a non-root user\n\n");
 		exit(EACCES);
 	}
